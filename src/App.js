48018c70e1037c6bc836b27011ff2c8b8b416a07
jeonj <jeon.jongmin@gmail.com>
2022-08-22 21:50:27 +0900

    07 영화 앱 다듬기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
   state = {
@@ -21,11 +22,14 @@ class App extends React.Component{
 
   render(){
     const { isLoading, movies } = this.state;
-    return <div>
-      {isLoading 
-      ? 'Loading...' 
-      : movies.map((movie) => {
-        return (
+    return (
+    <selection className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__test">Loading...</span>
+        </div>
+      ) : (<div className='movies'>
+        {movies.map(movie => (
           <Movie
             key={movie.id}
             id = {movie.id}
@@ -33,9 +37,11 @@ class App extends React.Component{
             title = {movie.title}
             summary = {movie.summary}
             poster = {movie.medium_cover_image}
-          /> );
-      })}
-    </div>;
+            genres = {movie.genres}
+          /> ))}
+      </div>)}
+    </selection>
+    );
   }
 }
 
